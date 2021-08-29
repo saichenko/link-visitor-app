7d32a9f3a7bab5e23c7ef0fa4909cbe95694b96e
saichenko <reallinuxforlinux@gmail.com>
2021-08-29 20:07:09 +0700

    Init

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import local
+
+
+def bash():
+    local("docker exec -it $(docker ps | grep server_ | awk '{{ print $1 }}')"
+          " bash")
+
+
+def shell():
+    local("docker exec -it $(docker ps | grep server_ | awk '{{ print $1 }}')"
+          " python manage.py shell")
+
+
+def dev():
+    local("docker-compose run --rm --service-ports server")
+
+
+def pip_compile_dev():
+    local("docker exec -i $(docker ps | grep server_ | awk '{{ print $1 }}') "
+          "pip-compile --generate-hashes requirements/development.in")
+
+
+def pip_sync_dev():
+    local("docker exec -i $(docker ps | grep server_ | awk '{{ print $1 }}') "
+          "pip-sync requirements/development.txt")
+
+
+def pip_compile_prod():
+    local("docker exec -i $(docker ps | grep server_ | awk '{{ print $1 }}') "
+          "pip-compile --generate-hashes requirements/production.in")
+
+
+def pip_sync_prod():
+    local("docker exec -i $(docker ps | grep server_ | awk '{{ print $1 }}') "
+          "pip-sync requirements/production.txt")
+
+
+def kill():
+    local("docker kill $(docker ps -q)")
