2ea29aa1014408b016db64607009daf35bf80f67
saichenko <reallinuxforlinux@gmail.com>
2021-08-30 03:01:30 +0700

    Create VisitedLinksSerializer

---
+++ server/apps/links/api/serializers.py
@@ -0,0 +1,50 @@
+import typing
+
+from django.core.cache import cache
+from django.utils.timezone import datetime
+from rest_framework import serializers
+from tldextract import extract
+
+
+def get_current_timestamp() -> int:
+    """Return current timestamp with rounding."""
+    return int(datetime.now().timestamp())
+
+
+def extract_domains(urls: typing.Iterable) -> typing.Tuple[str]:
+    """Return extracted domains from urls."""
+    extracted_urls = map(extract, urls)
+    domains = map(lambda url: url.registered_domain, extracted_urls)
+    return tuple(domains)
+
+
+class VisitedLinksSerializer(serializers.Serializer):
+    """Serializer for many links."""
+
+    links = serializers.ListField(
+        child=serializers.CharField()
+    )
+    visited_at = serializers.HiddenField(
+        default=get_current_timestamp
+    )
+
+    def create(self, validated_data: dict) -> dict:
+        """Save links to Redis.
+
+        Saving patterns for links:
+            `domain:<pk>:name`: Domain name. For example `google.com`.
+            `domain:<pk>:visited_at`: Since epoch timestamp of visiting.
+        """
+        domains = extract_domains(validated_data['links'])
+        for domain in domains:
+            primary_key = cache.incr('domain:id')
+            pattern = f'domain:{primary_key}:'
+            cache.set(f'{pattern}name', domain)
+            cache.set(
+                f'{pattern}visited_at',
+                validated_data['visited_at']
+            )
+        return validated_data
+
+    def update(self, instance, validated_data):
+        """Escape warning."""
