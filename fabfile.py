caff6746449206a9fe3709de046a2c34953f6257
Vlad Saichenko <38493865+saichenko@users.noreply.github.com>
2021-08-31 02:57:33 +0700

    Add test running command

--- fabfile.py
+++ fabfile.py
@@ -25,5 +25,10 @@ def pip_compile_prod():
           "pip-compile --generate-hashes requirements/production.in")
 
 
+def test():
+    local("docker exec -it $(docker ps | grep server_ | awk '{{ print $1 }}') "
+          "pytest -v")
+
+
 def kill():
     local("docker kill $(docker ps -q)")
