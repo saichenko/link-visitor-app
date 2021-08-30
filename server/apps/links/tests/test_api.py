4898efa7c11f65f59aa014ae7ef3f9defecb2894
saichenko <reallinuxforlinux@gmail.com>
2021-08-30 22:06:34 +0700

    Create test case for visited domains filtering

--- server/apps/links/tests/test_api.py
+++ server/apps/links/tests/test_api.py
@@ -1,11 +1,10 @@
+import random
 import uuid
 
 import pytest
 from django.core.cache import cache
 from django.urls import reverse
 from rest_framework.test import APIClient
-
-from apps.links.api.serializers import get_current_timestamp
 
 
 @pytest.fixture(scope='module')
@@ -20,7 +19,7 @@ def domain():
     primary_key = cache.incr('domains-id')
     pattern = f'domain:{primary_key}:'
     name = f'{uuid.uuid4()}.com'
-    timestamp = get_current_timestamp()
+    timestamp = random.randint(1, 10000000)
     data = {
         f'{pattern}name': name,
         f'{pattern}visited_at': timestamp
@@ -46,5 +45,17 @@ def test_visited_domains_fetching(api_client, domain):
     """Test GET request returns created domains."""
     url = reverse('api:visited-domains-list')
     response = api_client.get(url)
+    assert response.status_code == 200
+    assert domain['name'] in response.data['domains']
+    assert response.data['status'] == 'ok'
+
+
+def test_visited_domains_filtering(api_client, domain):
+    """Test visited domains Query param filtering with `from` and `to`."""
+    timestamp = domain['timestamp']
+    response = api_client.get(
+        reverse('api:visited-domains-list'),
+        {'from': timestamp - 1, 'to': timestamp + 1}
+    )
     assert response.status_code == 200
     assert domain['name'] in response.data['domains']
